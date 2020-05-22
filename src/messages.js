ac6e307353e482d27ce8290bca784f11fc9fb64f
MaurizioFranco <maurizio.franco@ymail.com>
2020-05-22 15:23:25 +0200

    Added messages for login authentication KO

--- src/messages.js
+++ src/messages.js
@@ -1,4 +1,5 @@
-
+//LOGIN
+export const LOGIN_AUTHENTICATION_KO = "Email e/o password errata/e. Si prega di riprovare." ;
 
 export const CANDIDATE_SUCCESSFULLY_DELETED_MESSAGE = "Candidato correttamente cancellato: " ;
 export const CANDIDATE_NOT_DELETED_MESSAGE = "Attenzione! Non è stato possibile cancellare il candidato " ;
