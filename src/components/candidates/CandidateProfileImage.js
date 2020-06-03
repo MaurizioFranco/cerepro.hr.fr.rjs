26826e2a2a88c1f5c6d117817b41cc1a40cb0288
MaurizioFranco <maurizio.franco@ymail.com>
2020-06-03 22:11:18 +0200

    Inserted process.env.PUBLIC_URL in img profile image rendering

--- src/components/candidates/CandidateProfileImage.js
+++ src/components/candidates/CandidateProfileImage.js
@@ -7,7 +7,7 @@ class CandidateProfileImage extends Component {
 	render () {
 		if (this.props.img!==null) {
 			return (
-			    <img className="profileImages" src={Constants.CANDIDATE_PROFILE_IMAGES_RELATIVE_FOLDER+this.props.img} alt=''/>
+			    <img className="profileImages" src={process.env.PUBLIC_URL+Constants.CANDIDATE_PROFILE_IMAGES_RELATIVE_FOLDER+this.props.img} alt=''/>
 		)} else {
 	    	return ('')
 	    }
