bff0ab549951995b1c785db96ea8124c27c85394
MaurizioFranco <maurizio.franco@ymail.com>
2020-11-19 10:23:36 +0100

    Added remote backend ref

--- src/env.js
+++ src/env.js
@@ -4,6 +4,10 @@
 //const ENV = "dev" ;
 //export const BACKEND_API_PREFIX = BACKEND_HOST_SERVICE + APPLICATION_CONTEXT + ENV ;
 //for local development....uncomment the above
-export const HOST = "localhost" ;
-export const PORT = "8080" ;
-export const APPLICATION_CONTEXT = "/cerepro.hr.backend" ;
+//export const HOST = "localhost" ;
+//export const PORT = "8080" ;
+//export const APPLICATION_CONTEXT = "/cerepro.hr.backend" ;
+export const HOST = "centauri.proximainformatica.com" ;
+export const PORT = "80" ;
+export const APPLICATION_CONTEXT = "/cerepro.hr.backend/dev" ;
+
