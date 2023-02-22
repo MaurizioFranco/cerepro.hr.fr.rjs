27c8a8159477faf2969514896442ba6b445c5d10
MaurizioFranco <maurizio.franco@ymail.com>
2023-02-22 22:35:19 +0100

    Added isBodyAJSON parameter to handle body payload in json for POST and PUT without attachements

--- src/commons.js
+++ src/commons.js
@@ -13,9 +13,20 @@ export function getAuthorizationToken(email, password) {
     debugMessage("###"+authdata+"###");
     return authdata;
 }
-export function getAuthorizationHeaderFromToken(token) {
-	debugMessage("getAuthorizationHeader - START - token: " + token);
-	return { 'Authorization': 'Basic ' + token };
+export function getAuthorizationHeaderFromToken(token, isBodyAJSON) {
+	let authToken = null ;
+	if (isBodyAJSON!==null && isBodyAJSON) {
+        authToken = { 
+			'Authorization': 'Basic ' + token,
+			'Content-Type': 'application/json'
+		} ;
+	} else {
+		authToken = { 
+			'Authorization': 'Basic ' + token 
+		} ;
+	}
+	debugMessage("getAuthorizationHeader - START - authToken: " + authToken);
+	return authToken;
 }
 
 export function getUserLoggedId () {
@@ -25,38 +36,71 @@ export function getUserLoggedId () {
 	
 }
 
-export function executeFetch (uri, method, callbackFunction, callbackFunctionKO, body) {
+export function executeFetch (uri, method, successCallbackFunction, callbackFunctionKO, body, isBodyAJSON) {
 	debugMessage("Commons.executeFetch - START - uri: " + uri);
 	let token = sessionStorage.getItem('headerToken');
-	let headerToken = getAuthorizationHeaderFromToken(token);
-	executeFetchWithHeader (uri, method, headerToken, callbackFunction, callbackFunctionKO, body)
+	// let headerToken = null ;
+	// if (isBodyAJSON!==null && isBodyAJSON) {
+    let headerToken = getAuthorizationHeaderFromToken(token, isBodyAJSON);
+	// } else {
+        // headerToken = getAuthorizationHeaderFromToken(token);
+	// }
+	executeFetchWithHeader (uri, method, headerToken, successCallbackFunction, callbackFunctionKO, body)
 	
 }
 
-export function executeFetchWithHeader (uri, method, headerToken, callbackFunction, callbackFunctionKO, body) {
+export function executeFetchWithHeader (uri, method, headerToken, successCallbackFunction, callbackFunctionKO, body) {
 	debugMessage("Commons.executeFetchWithHeader - START - uri: " + uri);
-	
+	debugMessage(`Commons.executeFetchWithHeader - DEBUG - body: ${body}`);
+	debugMessage(body);
+	debugMessage(`Commons.executeFetchWithHeader - DEBUG - method: ${method}  - uri: ${uri}`);
+	debugMessage(body);
 	trackPromise(
 		fetch(uri , {
 	        method: method,
-	        body: body,
+			body: body,
+			
 	        headers: headerToken
 	                  },)
-		  .then((response) => {
-		      if(!response.ok) {
-		    	  console.warn(response.status); // Will show you the status
-		    	  //throw new Error(response.status);
-		      } else if (method==="DELETE") {
-		    	  return "" ;
-		      } else return response.json();
-		  })
+		//   .then((response) => {
+			.then(r =>  r.json().then(data => ({status: r.status, body: data})))
+			//   console.log(response);
+			// //   if (method==="POST" && response.status===201) {
+			// // 	  successCallbackFunction(response);
+			// //   } else if (response.status===200) {
+			// // 	  successCallbackFunction(response.json());
+			// //   } else {//ERROR
+			// // 	callbackFunctionKO();
+			// //   }
+		    // //   if(!response.ok) {
+		    // // 	  console.warn(response.status); // Will show you the status
+		    // // 	  //throw new Error(response.status);
+		    // //   } else if (method==="DELETE") {
+		    // // 	  return "" ;
+			// //   } else return response.json();
+			// let responseData = response.json() 
+			// //   return response.json();
+			// return (responseData) ;
+			//     //   if (method==="POST" && response.status===201) {
+			// 	// 	  successCallbackFunction(responseData);
+			// 	//   } else if (response.status===200) {
+			// 	// 	  successCallbackFunction(responseData);
+			// 	//   } else {//ERROR
+			// 	// 	callbackFunctionKO(responseData);
+			// 	//   }
+		//   })
 		  .then((data) => {
 			  debugMessage("Commons.executeFetchWithHeader - DEBUG - data: " + data);
-			  if (data!==undefined) {
-				  callbackFunction(data);
-			  } else {
-				  callbackFunctionKO();
-			  }
+			  if (data.status===200||data.status===201) {
+					  successCallbackFunction(data.body);
+				  } else {//ERROR
+					callbackFunctionKO(data.body);
+				  }
+			//   if (data!==undefined) {
+			// 	successCallbackFunction(data);
+			//   } else {
+			// 	  callbackFunctionKO(data);
+			//   }
 		  })
     );
 }
