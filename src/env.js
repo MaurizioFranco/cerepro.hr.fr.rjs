93c78f509e7189d2105ff2bd39ce70dec7c50e76
Academy3 <Academy3@192.168.56.1>
2023-03-13 11:54:09 +0100

    Commit

--- src/env.js
+++ src/env.js
@@ -1,4 +1,13 @@
 
+// export const HOST = "centauri.proximainformatica.com" ;
+// export const PORT = "80" ;
+// export const APPLICATION_CONTEXT = "/cerepro.hr.backend/dev" ;
+
 export const HOST = "centauri.proximainformatica.com" ;
 export const PORT = "80" ;
-export const APPLICATION_CONTEXT = "/cerepro.hr.backend/dev" ;
+export const APPLICATION_CONTEXT = "/repro.be/dev" ;
+
+//local
+// export const HOST = "localhost" ;
+// export const PORT = "8080" ;
+// export const APPLICATION_CONTEXT = "/cerepro.hr.backend" ;
