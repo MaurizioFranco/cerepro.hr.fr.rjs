b0f4083b0e69a416cf59129f5d36b3ea50b98395
MaurizioFranco <maurizio.franco@ymail.com>
2023-03-13 12:02:21 +0100

    merged and solved issue

--- src/env.js
+++ src/env.js
@@ -1,4 +1,7 @@
-
 export const HOST = "centauri.proximainformatica.com" ;
 export const PORT = "80" ;
-export const APPLICATION_CONTEXT = "/cerepro.hr.backend/dev" ;
+export const APPLICATION_CONTEXT = "/repro.be/dev" ;
+//local
+// export const HOST = "localhost" ;
+// export const PORT = "9090" ;
+// export const APPLICATION_CONTEXT = "/cerepro.hr.backend" ;
