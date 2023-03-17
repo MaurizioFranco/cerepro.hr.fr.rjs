61345aa4831fe85a1c509f0c1c016dcb06610214
Giammarco93 <giammarco.lucchetti@hotmail.it>
2023-03-17 16:19:50 +0100

    unificato il toast pdf

--- src/components/question/Question.js
+++ src/components/question/Question.js
@@ -141,10 +141,11 @@ class Question extends Component {
     }
 
     generateSuccess = () => {
+        Commons.operationSuccess();
         this.reloadData();
-        toast.success("PDF regenerated", {
-            position: toast.POSITION.BOTTOM_LEFT,
-        });
+        // toast.success("PDF regenerated", {
+        //     position: toast.POSITION.BOTTOM_LEFT,
+        // });
     }
 
     generateFailed = () => {
@@ -160,7 +161,7 @@ class Question extends Component {
         // console.log(surveyReplyId)
         // let token = sessionStorage.getItem('headerToken');
         // console.log("token: " + token)
-        Commons.executeFetch(Constants.FULL_PDF_END + surveyReplyId, 'POST', this.generateSuccess, this.generateFailed)
+        Commons.executeFetch(Constants.FULL_PDF_END + surveyReplyId, 'POST', this.generateSuccess, Commons.operationError)
         // fetch(Constants.FULL_PDF_END + surveyReplyId,
         //     {
         //         method: 'POST',
