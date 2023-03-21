5e65467c8e8b6d368f047cb20e62bfe2d08580ee
Antonio <Antonio@LAPTOP-GPM0CA74>
2023-03-21 11:04:58 +0100

    New frontend field added in constants

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,7 @@
 import * as Environment from './env.js';
 
 export const BACKEND_API_PREFIX = "http://" + Environment.HOST + ":" + Environment.PORT + Environment.APPLICATION_CONTEXT ;
+export const FRONTEND_API_PREFIX = "http://" + Environment.FRONTEND_HOST + Environment.FRONTEND_APPLICATION_CONTEXT ;
 
 export const CANDIDATE_PROFILE_IMAGES_RELATIVE_FOLDER = "/canimg/" ;
 export const CANDIDATE_PROFILE_CV_RELATIVE_FOLDER = "/cancv/" ;
