11a288fba433eae8d9e134cd5eea57a5d56763e9
MaurizioFranco <maurizio.franco@ymail.com>
2023-03-21 17:13:24 +0100

    restored old CandidateList component

--- src/components/candidates/CandidatesView.js
+++ src/components/candidates/CandidatesView.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import CandidatesList from './CandidatesList.js';
+// import CandidatesList from './CandidatesList.js';
+import CandidateList from './CandidateList.js';
 //import './candidates/candidates.css';
 
 class CandidatesView extends Component {
 	render() {
 		return (
-				  <CandidatesList/>
+				  <CandidateList/>
 					
 		);
 	}
